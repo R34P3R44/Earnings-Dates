3a85ab22bbf3f15971252d80dae5090e7dc9f7e5
R34P3R44 <t.adrian88@gmail.com>
2021-11-22 23:15:11 +0000

    Add new method with url request

--- GetEarningsDates.py
+++ GetEarningsDates.py
@@ -2,3 +2,10 @@ import datetime
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+def GetEarningsDates():
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    }
+    url = 'https://www.marketwatch.com/tools/earningscalendar'
+    print('Connecting to marketwatch.com')
